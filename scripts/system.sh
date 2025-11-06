86107900ce53505fe399e63b545c25dd4bf9dcc5
Julen Bohoyo <73544256+julenbhy@users.noreply.github.com>
2025-11-06 12:54:54 +0100

    Update system.sh

--- scripts/system.sh
+++ scripts/system.sh
@@ -3,20 +3,20 @@
 
 set -e
 
-echo "[1/7] Disabling GUI mode... Will take effect after reboot."
-#sudo systemctl set-default multi-user.target
+echo "[1/4] Disabling GUI mode... Will take effect after reboot."
+sudo systemctl set-default multi-user.target
 
-echo "[2/7] Setting MAXN_SUPER power mode..."
+echo "[2/4] Setting MAXN_SUPER power mode..."
 sudo nvpmodel -m 2
 sudo nvpmodel -q # Verify current power mode
 
-echo "[3/7] Updating and upgrading system packages..."
+echo "[3/4] Updating and upgrading system packages..."
 sudo apt-get update
 sudo apt-get full-upgrade -y
 
 # Can't disable swap before building libtorch, as it may require more memory
-#echo "Disabling swap..."
-#sudo swapoff -a
-#sudo sed -i.bak '/ swap / s/^/#/' /etc/fstab
+echo "[4/4] Disabling swap..."
+sudo swapoff -a
+sudo sed -i.bak '/ swap / s/^/#/' /etc/fstab
 
 echo "System configuration done"
